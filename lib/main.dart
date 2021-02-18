b68a419f4c9b94665bce9cfce7e0bd079a9a811e
Manas Kaul <kaulmanascocid@gmail.com>
2021-02-18 19:04:04 +0530

    list all installed apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_apps/device_apps.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,6 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: HomePage(),
     );
   }
@@ -17,16 +20,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ApplicationWithIcon> allApps = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getInstalledApps();
+  }
+
+  getInstalledApps() async {
+    await DeviceApps.getInstalledApplications(
+      includeAppIcons: true,
+      includeSystemApps: false,
+      onlyAppsWithLaunchIntent: true,
+    ).then((value) {
+      for (int i = 0; i < value.length; i++) {
+        allApps.add(value[i] as ApplicationWithIcon);
+      }
+    });
+    allApps.sort(
+            (x, y) => x.appName.toLowerCase().compareTo(y.appName.toLowerCase()));
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter App"),
+        centerTitle: true,
+        title: Text("Android Privacy"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Hello World !"),
-        ),
+      body: Container(
+        alignment: Alignment.center,
+        child: allApps != null
+            ? ListView.builder(
+          itemCount: allApps.length,
+          itemBuilder: (BuildContext context, int index) {
+            ApplicationWithIcon app = allApps[index];
+            return new ListTile(
+              title: Text(app.appName),
+              subtitle: Text(app.versionName),
+              leading: CircleAvatar(
+                backgroundImage: MemoryImage(app.icon),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios_sharp,
+                // color: Colors.black,
+              ),
+              onTap: () {},
+            );
+          },
+        )
+            : Container(),
       ),
     );
   }
