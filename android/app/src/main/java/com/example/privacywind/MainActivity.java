1448fa731d343935fd6ef7d9f7d1b797dba885ec
Manas Kaul <kaulmanascocid@gmail.com>
2021-02-18 17:22:03 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/privacywind/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.privacywind;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello World !"),
+        ),
+      ),
+    );
+  }
+}
