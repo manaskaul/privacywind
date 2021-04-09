759280bd0e5b97fb72b0930d10e2a0b84c08175c
Manas Kaul <kaulmanascocid@gmail.com>
2021-04-09 18:24:45 +0530

    fixed: handle error if search result is empty

--- lib/app_search/app_search.dart
+++ lib/app_search/app_search.dart
@@ -43,7 +43,7 @@ class _AppSearchState extends State<AppSearch> {
                         .then((value) {
                       searchResult = value;
                     });
-                    if (searchResult.isEmpty) {
+                    if (searchResult == null || searchResult.isEmpty) {
                       setState(() {
                         isLoading = false;
                         Scaffold.of(context).showSnackBar(
@@ -82,72 +82,90 @@ class _AppSearchState extends State<AppSearch> {
           // mainAxisSize: MainAxisSize.min,
           children: [
             AppCategories(),
-            gotAppList
+            isLoading
                 ? Expanded(
-                    child: ListView.builder(
-                      itemCount: searchResult.length,
-                      itemBuilder: (context, index) {
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Container(
-                              child: new ListTile(
-                                title: Text(searchResult[index].title),
-                                subtitle: Text(searchResult[index].developer),
-                                leading: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      "${searchResult[index].icon}"),
-                                  backgroundColor: Colors.transparent,
-                                ),
-                                trailing: Icon(
-                                  Icons.arrow_forward_ios_sharp,
-                                  size: 15.0,
-                                  // color: Colors.black,
-                                ),
-                                onTap: () {
-                                  debugPrint("${searchResult[index].appId}");
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          SearchAppPermissionList(
-                                        packageName: searchResult[index].appId,
-                                        appName: searchResult[index].title,
-                                        iconString: searchResult[index].icon,
-                                        playURL: searchResult[index].url,
-                                        appSummary: searchResult[index].summary,
-                                      ),
-                                    ),
-                                  );
-                                },
-                              ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Text(
+                            "Looking for results...",
+                            style: TextStyle(
+                              fontSize: 20.0,
                             ),
-                            Container(
-                              width: MediaQuery.of(context).size.width * 0.9,
-                              child: Divider(thickness: 1.0),
-                            ),
-                          ],
-                        );
-                      },
+                          ),
+                        ),
+                        Loading(),
+                      ],
                     ),
                   )
-                : isLoading
-                    ? Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              child: Text(
-                                "Looking for results...",
-                                style: TextStyle(
-                                  fontSize: 20.0,
+                : gotAppList
+                    ? searchResult == null
+                        ? Expanded(
+                            child: Center(
+                              child: Container(
+                                child: Text(
+                                  "There was an error getting the app list",
                                 ),
                               ),
                             ),
-                            Loading(),
-                          ],
-                        ),
-                      )
+                          )
+                        : Expanded(
+                            child: ListView.builder(
+                              itemCount: searchResult.length,
+                              itemBuilder: (context, index) {
+                                return Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Container(
+                                      child: new ListTile(
+                                        title: Text(searchResult[index].title),
+                                        subtitle:
+                                            Text(searchResult[index].developer),
+                                        leading: CircleAvatar(
+                                          backgroundImage: NetworkImage(
+                                              "${searchResult[index].icon}"),
+                                          backgroundColor: Colors.transparent,
+                                        ),
+                                        trailing: Icon(
+                                          Icons.arrow_forward_ios_sharp,
+                                          size: 15.0,
+                                          // color: Colors.black,
+                                        ),
+                                        onTap: () {
+                                          debugPrint(
+                                              "${searchResult[index].appId}");
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  SearchAppPermissionList(
+                                                packageName:
+                                                    searchResult[index].appId,
+                                                appName:
+                                                    searchResult[index].title,
+                                                iconString:
+                                                    searchResult[index].icon,
+                                                playURL:
+                                                    searchResult[index].url,
+                                                appSummary:
+                                                    searchResult[index].summary,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                    Container(
+                                      width: MediaQuery.of(context).size.width *
+                                          0.9,
+                                      child: Divider(thickness: 1.0),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                          )
                     : Expanded(
                         child: Center(
                           child: Container(
@@ -163,3 +181,83 @@ class _AppSearchState extends State<AppSearch> {
     );
   }
 }
+
+/*
+
+gotAppList
+    ? Expanded(
+        child: ListView.builder(
+          itemCount: searchResult.length,
+          itemBuilder: (context, index) {
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  child: new ListTile(
+                    title: Text(searchResult[index].title),
+                    subtitle: Text(searchResult[index].developer),
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "${searchResult[index].icon}"),
+                      backgroundColor: Colors.transparent,
+                    ),
+                    trailing: Icon(
+                      Icons.arrow_forward_ios_sharp,
+                      size: 15.0,
+                      // color: Colors.black,
+                    ),
+                    onTap: () {
+                      debugPrint("${searchResult[index].appId}");
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              SearchAppPermissionList(
+                            packageName: searchResult[index].appId,
+                            appName: searchResult[index].title,
+                            iconString: searchResult[index].icon,
+                            playURL: searchResult[index].url,
+                            appSummary: searchResult[index].summary,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Divider(thickness: 1.0),
+                ),
+              ],
+            );
+          },
+        ),
+      )
+    : isLoading
+        ? Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Text(
+                    "Looking for results...",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                Loading(),
+              ],
+            ),
+          )
+        : Expanded(
+            child: Center(
+              child: Container(
+                child: Text(
+                  "Search an App to get its permission list",
+                ),
+              ),
+            ),
+          ),
+
+*/
--- lib/app_search/search_category/app_list.dart
+++ lib/app_search/search_category/app_list.dart
@@ -59,57 +59,61 @@ class _AppListState extends State<AppList> {
                   Loading(),
                 ],
               )
-            : ListView.builder(
-                itemCount: searchResult.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        child: new ListTile(
-                          title: Text(searchResult[index].title),
-                          subtitle: Text(searchResult[index].developer),
-                          leading: CircleAvatar(
-                            backgroundImage:
-                                NetworkImage("${searchResult[index].icon}"),
-                            backgroundColor: Colors.transparent,
-                          ),
-                          trailing: Icon(
-                            Icons.arrow_forward_ios_sharp,
-                            size: 15.0,
-                            // color: Colors.black,
-                          ),
-                          onTap: () async {
-                            debugPrint("${searchResult[index].appId}");
-                            var res = await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => AppDetails(
-                                  packageName: searchResult[index].appId,
-                                  appName: searchResult[index].title,
-                                  iconString: searchResult[index].icon,
-                                  playURL: searchResult[index].url,
-                                  appSummary: searchResult[index].summary,
-                                  compareListSize: compareList.length,
-                                ),
+            : searchResult != null
+                ? ListView.builder(
+                    itemCount: searchResult.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            child: new ListTile(
+                              title: Text(searchResult[index].title),
+                              subtitle: Text(searchResult[index].developer),
+                              leading: CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage("${searchResult[index].icon}"),
+                                backgroundColor: Colors.transparent,
                               ),
-                            );
-                            if (res != null) {
-                              setState(() {
-                                compareList.add(res);
-                              });
-                            }
-                          },
-                        ),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.9,
-                        child: Divider(thickness: 1.0),
-                      ),
-                    ],
-                  );
-                },
-              ),
+                              trailing: Icon(
+                                Icons.arrow_forward_ios_sharp,
+                                size: 15.0,
+                                // color: Colors.black,
+                              ),
+                              onTap: () async {
+                                debugPrint("${searchResult[index].appId}");
+                                var res = await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => AppDetails(
+                                      packageName: searchResult[index].appId,
+                                      appName: searchResult[index].title,
+                                      iconString: searchResult[index].icon,
+                                      playURL: searchResult[index].url,
+                                      appSummary: searchResult[index].summary,
+                                      compareListSize: compareList.length,
+                                    ),
+                                  ),
+                                );
+                                if (res != null) {
+                                  setState(() {
+                                    compareList.add(res);
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            child: Divider(thickness: 1.0),
+                          ),
+                        ],
+                      );
+                    },
+                  )
+                : Center(
+                    child: Text("There was an error getting the app list"),
+                  ),
       ),
       floatingActionButton: InkWell(
         child: FloatingActionButton.extended(
@@ -140,7 +144,8 @@ class _AppListState extends State<AppList> {
   }
 
   getFloatingActionButtonColor() {
-    if (compareList.length >= AppSearchConstants.MIN_COMPARE_APPS && compareList.length <= AppSearchConstants.MAX_COMPARE_APPS) {
+    if (compareList.length >= AppSearchConstants.MIN_COMPARE_APPS &&
+        compareList.length <= AppSearchConstants.MAX_COMPARE_APPS) {
       return Colors.lightBlue[700];
     } else {
       if (MediaQuery.of(context).platformBrightness == Brightness.light) {
